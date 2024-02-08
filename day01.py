f3536692bac47afe6539fb409fbe296761bbb115
pjy <nihoo4u@gmail.com>
2024-02-08 15:34:28 +0900

    calculate x

--- day01.py
+++ day01.py
@@ -1,16 +1,20 @@
-def add_it(x):
-    a.append(x)
-def insert_it(x,y):
-    a.append(None)
-    for i in range(len(a)-1,x,-1):
-        a[i],a[i-1]=a[i-1],a[i]
-    a[x]=y
+coeff=[5,-2,0,1,2,4]
+string=''
+term=len(coeff)-1
+for i in range(len(coeff)): #0->3
+    if i!=0 and coeff[i]>0:
+        string+="+"
 
+    if coeff[i]!=0:
+        string+=f"{coeff[i]}x^{term}"
+    term-=1
+print(string)
 
-
-
-
-a=[1,2,3,4]
-insert_it(2,10)
-print(a)
+term=len(coeff)-1
+result=0
+x=int(input("input the number"))
+for i in range(len(coeff)):
+    result+=coeff[i]*x**term
+    term-=1
+print(result)
 

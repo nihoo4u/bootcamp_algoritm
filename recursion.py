e600a33c2305770fa9b979aa2851e4fee632d25e
pjy <nihoo4u@gmail.com>
2024-02-14 17:24:26 +0900

    memoization

--- recursion.py
+++ recursion.py
@@ -1,27 +1,15 @@
-# def fibo(n):
-#     if n<=1:
-#         return n
-#
-#     return fibo(n-1)+fibo(n-2)
-#
-# for i in range(2020):
-#     print(fibo(i), end=' ')
-
-a=[0]*40
-a[1]=1
 def fibo(n):
-    for i in range(2,n+1):
-        a[i]=a[i-1]+a[i-2]
+    if memo[n] is not None:
+        return memo[n]
+    if n<=1:
+        return n
 
-def fibo_repetition(n):
-    c=0
-    d=1
 
-    for _ in range(n):
+    x= fibo(n-1)+fibo(n-2)
+    memo[n]=x
+    return x
 
-        c,d=d,c+d
-    return c
+memo=[None]*30
 
-fibo(30)
-print(a[10])
-print(fibo_repetition(10))
+print(fibo(5))
+print(memo[5])

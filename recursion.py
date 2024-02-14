11e2a7ee34557705f05b462f1a08dc971fbb7601
pjy <nihoo4u@gmail.com>
2024-02-14 16:55:10 +0900

    recursion vs repetition

--- recursion.py
+++ recursion.py
@@ -1,8 +1,27 @@
+# def fibo(n):
+#     if n<=1:
+#         return n
+#
+#     return fibo(n-1)+fibo(n-2)
+#
+# for i in range(2020):
+#     print(fibo(i), end=' ')
+
+a=[0]*40
+a[1]=1
 def fibo(n):
-    if n<=1:
-        return n
+    for i in range(2,n+1):
+        a[i]=a[i-1]+a[i-2]
 
-    return fibo(n-1)+fibo(n-2)
+def fibo_repetition(n):
+    c=0
+    d=1
 
-for i in range(2020):
-    print(fibo(i), end=' ')
+    for _ in range(n):
+
+        c,d=d,c+d
+    return c
+
+fibo(30)
+print(a[10])
+print(fibo_repetition(10))

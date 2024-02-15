d3f3c62a2c08838f8ccf0263abe7d000f810dff7
pjy <nihoo4u@gmail.com>
2024-02-15 17:07:12 +0900

    file_divided using import,fromgit add sort.py functions_zip.py

---
+++ functions_zip.py
@@ -0,0 +1,8 @@
+def quick_sort(x):
+    if len(x)<2:
+        return x
+    pivot=x[0]
+    remn=x[1:]
+    less=[z for z in remn if z<=pivot]
+    more=[z for z in remn if z>pivot]
+    return quick_sort(less) + [pivot] + quick_sort(more)

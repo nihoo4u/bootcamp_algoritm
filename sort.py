910e032272152907c9a476c818f1d5cf0b6bce64
pjy <nihoo4u@gmail.com>
2024-02-15 17:05:31 +0900

    file_divided using import,from

--- sort.py
+++ sort.py
@@ -31,7 +31,9 @@
 # print(a)
 
 ## python optimized quick_sort()
-# import random
+import time
+import random
+import functions_zip
 # def quick_sort(x):
 #     if len(x)<2:
 #         return x
@@ -40,31 +42,34 @@
 #     less=[z for z in remn if z<=pivot]
 #     more=[z for z in remn if z>pivot]
 #     return quick_sort(less) + [pivot] + quick_sort(more)
-# a=[random.randint(0,200) for _ in range(10)]
-# print(a, len(a))
-# b=quick_sort(a)
-# print(b,len(b))
-
-import random
-def bubble_sort():
-    for j in range(len(a)-1):
-        ch=False
-        for i in range(1,len(a)-j):
-            if a[i]< a[i-1]:
-                a[i],a[i-1]=a[i-1],a[i]
-                ch=True
-        if  not ch:
-            break
-    return a
-
+start=time.time()
 a=[random.randint(0,200) for _ in range(10)]
-b=sorted(a)
-print('original',a)
-bubble_sort()
-print('by bubble',a)
-print('real answer',a)
-if a==b:
-    print('good')
-else:
-    print('suck')
+print(a, len(a))
+b=functions_zip.quick_sort(a)
+print(b,len(b))
+end=time.time()
+print(end-start)
+
+# import random
+# def bubble_sort():
+#     for j in range(len(a)-1):
+#         ch=False
+#         for i in range(1,len(a)-j):
+#             if a[i]< a[i-1]:
+#                 a[i],a[i-1]=a[i-1],a[i]
+#                 ch=True
+#         if  not ch:
+#             break
+#     return a
+#
+# a=[random.randint(0,200) for _ in range(10)]
+# b=sorted(a)
+# print('original',a)
+# bubble_sort()
+# print('by bubble',a)
+# print('real answer',a)
+# if a==b:
+#     print('good')
+# else:
+#     print('suck')
 

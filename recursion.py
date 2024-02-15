5309e9aec12262793e5f067ba661be29e81a9b74
pjy <nihoo4u@gmail.com>
2024-02-15 11:21:20 +0900

    self_made trsnsitioning to binary code

--- recursion.py
+++ recursion.py
@@ -1,15 +1,47 @@
-def fibo(n):
-    if memo[n] is not None:
-        return memo[n]
-    if n<=1:
-        return n
+# def fibo(n):
+#     if memo[n] is not None:
+#         return memo[n]
+#     if n<=1:
+#         return n
+#
+#
+#     x= fibo(n-1)+fibo(n-2)
+#     memo[n]=x
+#     return x
+#
+# memo=[None]*50
+#
+#
+# print(fibo(5))
+# print(memo[5])
+
+## 함수 선언 부분 ##
+# def palindrome(pStr) :
+#     if len(pStr) <= 1 :
+#         return True
+#
+#     if pStr[0] != pStr[-1] :
+#         return False
+#
+#     return palindrome(pStr[1:len(pStr)-1])
 
 
-    x= fibo(n-1)+fibo(n-2)
-    memo[n]=x
-    return x
+## 전역 변수 선언 부분 ##
+# strAry = ["reaver", "kayak", "Borrow or rob", "주유소의 소유주", "야 너 이번주 주번이 너야", "살금 살금"]
 
-memo=[None]*30
+## 메인 코드 부분 ##
+# for testStr in strAry :
+# 	print(testStr, end = '--> ' )
+# 	testStr = testStr.lower().replace(' ','')
+# 	if palindrome(testStr) :
+# 		print('O')
+# 	else :
+# 		print('X')
 
-print(fibo(5))
-print(memo[5])
+def transfer(x,by):
+    if x<by:
+        return x
+
+
+    return str(transfer(x//by,by)) + str(x%by)
+print(transfer(100,2))

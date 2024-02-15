1644b278b1d8fb0c251842b9203a15aeb61afd56
pjy <nihoo4u@gmail.com>
2024-02-15 14:36:51 +0900

    python optimalized sort

--- sort.py
+++ sort.py
@@ -1,33 +1,46 @@
-## improved selection_sort ##
-# def selectionSort(ary) :
-#     n = len(ary)
-#     for i in range(0, n-1) :
-#         minIdx = i
-#         for k in range(i+1, n) :
-#             if (ary[minIdx] > ary[k]) :
-#                 ary[k],ary[minIdx]=ary[minIdx],ary[k]
-#
-#
-#     return ary
-#
-# ## 전역 변수 선언 부분 ##
-# dataAry = [188, 162, 168, 120, 50, 150, 177, 105]
-#
-# ## 메인 코드 부분 ##
-# print('정렬 전 -->', dataAry)
-# dataAry = selectionSort(dataAry)
-# print('정렬 후 -->', dataAry)
 
+## insert_sort
+# import random
+# def insert_sort(x):
+#     for i in range(1,len(x)):
+#         for j in range(i,0,-1):
+#             if x[j-1] > x[j]:
+#                 x[j-1],x[j]=x[j],x[j-1]
+#     return x
+#
+# a=[random.randint(0,200) for _ in range(10)]
+# print(a)
+# print(insert_sort(a))
+
+##choice_sort
+
+# import random
+# def choice_sort():
+#     for i in range(len(a)):
+#         index=i
+#         for j in range(i+1,len(a)):
+#             if a[index]> a[j]:
+#                 index=j
+#         a[index],a[i]=a[i],a[index]
+#
+#
+#
+# a=[random.randint(0,200) for _ in range(10)]
+# print(a)
+# choice_sort()
+# print(a)
+
+## python optimized quick_sort()
 import random
-def insert_sort(x):
-    for i in range(1,len(x)):
-        for j in range(i,0,-1):
-            if x[j-1] > x[j]:
-                x[j-1],x[j]=x[j],x[j-1]
-    return x
-
+def quick_sort(x):
+    if len(x)<2:
+        return x
+    pivot=x[0]
+    remn=x[1:]
+    less=[z for z in remn if z<=pivot]
+    more=[z for z in remn if z>pivot]
+    return quick_sort(less) + [pivot] + quick_sort(more)
 a=[random.randint(0,200) for _ in range(10)]
-print(a)
-print(insert_sort(a))
-
-
+print(a, len(a))
+b=quick_sort(a)
+print(b,len(b))
